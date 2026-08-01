87a8bcc10fdd4ab83d86167525a41e35fdeb0d93
luqmaan1005-jpg <luqmaan1005@gmail.com>
2026-08-01 15:42:35 +0200

    Make name and email optional in UserResult data class

--- android/src/main/java/com/webnativelqgl/UserResult.kt
+++ android/src/main/java/com/webnativelqgl/UserResult.kt
@@ -2,8 +2,8 @@ package com.webnativelqgl
 
 data class UserResult(
     val id: String,
-    val name: String,
-    val email: String,
+    val name: String?,
+    val email: String?,
     val photo: String?,
     val idToken: String
 )
