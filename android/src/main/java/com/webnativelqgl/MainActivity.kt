ef9145d002aedfc59d1ff16ab1d64cc4ecc50c6b
luqmaan1005-jpg <luqmaan1005@gmail.com>
2026-08-01 15:49:55 +0200

    Refactor onCreate and add Google Client ID
    Refactor onCreate method to improve readability and add Google Client ID.

--- android/src/main/java/com/webnativelqgl/MainActivity.kt
+++ android/src/main/java/com/webnativelqgl/MainActivity.kt
@@ -4,37 +4,29 @@ import android.os.Bundle
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
 
-
-    override fun onCreate(
-        savedInstanceState: Bundle?
-    ) {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         webView = WebView(this)
 
         setContentView(webView)
 
-
         webView.settings.javaScriptEnabled = true
 
 
         WebNativeLQGL(
             this,
-            webView
+            webView,
+            "662633670578-8t6mido2ddipl9oo7ts7of14lo9mvvas.apps.googleusercontent.com"
         ).install()
 
 
-        // Any website works here
         webView.loadUrl(
-            intent.getStringExtra("url")
-                ?: "https://example.com"
+            "https://example.com"
         )
-
     }
 }
